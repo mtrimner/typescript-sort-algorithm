9691d00be811adaf1131a7fb45dc3d1aa5dcec6f
mtrimner <matt.trimner@gmail.com>
2021-08-12 12:02:23 -0500

    refactored sorter to abstract class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 4, 19, -9, 9]);
 const charactersCollection = new CharactersCollection('Testing');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-33);
+linkedList.add(33);
+linkedList.add(87);
+linkedList.add(-3);
+
+linkedList.sort();
+linkedList.print();
